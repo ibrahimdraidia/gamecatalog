45e954d44dd372f860d29abde79d6e6a97af2c9a
ibrahim <me@ibrahimdraidia.com>
2016-10-17 14:59:42 +0200

    added db handlers ...

---
+++ administrator/src/db/Database.java
@@ -0,0 +1,47 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class Database 
+{
+	
+	private String _dbhost;
+	private String _dbname;
+	private String _username;
+	private String _password;
+	private String _url;
+	private Connection _connection;
+	
+	public Database()
+	{
+		_dbhost = "localhost";
+		_dbname = "";
+		_username = "root";
+		_password = "";
+		_url = "jdbc:mysql://"+_dbhost+"/"+_dbname+"?useSSL=false";
+	}
+	
+	public Connection getInstance()
+	{
+		if(_connection == null)
+		{
+			try {
+				Class.forName( "com.mysql.jdbc.Driver" );
+				Connection conn = DriverManager.getConnection(_url,_username,_password);
+				_connection = conn;
+
+			} catch (SQLException e) {
+				JOptionPane.showMessageDialog(null, "Error"+ e.getMessage(),"Mysql",JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			} catch (ClassNotFoundException e) {
+				JOptionPane.showMessageDialog(null, "Error"+ e.getMessage(),"Mysql",JOptionPane.ERROR_MESSAGE);
+			}
+		}		
+		return _connection;
+	}
+
+}
---
+++ administrator/src/interfaces/CrudInterface.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public interface CrudInterface {
+	
+	public void add();
+	public void update();
+	public void remove();
+	public void getAll();
+
+}
